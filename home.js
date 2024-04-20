b876f1a71eaa47a2c8532ecdd28e5a217f545696
Manjari Jain <manjjain@paypal.com>
2024-04-20 11:54:56 +0530

    handle left click of mouse

--- home.js
+++ home.js
@@ -1,32 +1,75 @@
-const canvas = document.getElementById("groundCanvas"); 
-const engine = new BABYLON.Engine(canvas, true); 
-
-
+// Buttons
 var drawMode = false;
 
 const drawButton = document.getElementById("draw");
-drawButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    drawMode = true;
+drawButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  drawMode = true;
 });
 
+// Canvas
+const canvas = document.getElementById("groundCanvas");
+const engine = new BABYLON.Engine(canvas, true);
 
+const coordinates = [];
 
-var createScene = function() {
-    var scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera("Camera",  0, Math.PI/4, 30, BABYLON.Vector3(0,0,0), scene);
-    camera.attachControl(canvas, true);
+var createScene = function () {
+  var scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.ArcRotateCamera(
+    "Camera",
+    0,
+    Math.PI / 4,
+    30,
+    BABYLON.Vector3(0, 0, 0),
+    scene
+  );
+  camera.attachControl(canvas, true);
 
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", { height:30, width:15 }, scene);
-    return scene;
-}
+  const ground = BABYLON.MeshBuilder.CreateGround(
+    "Ground",
+    { height: 30, width: 15 },
+    scene
+  );
 
+  // handle mouse clicks
+  scene.onPointerObservable.add((pointerInfo) => {
+    if (drawMode) {
+      switch (pointerInfo.type) {
+        case BABYLON.PointerEventTypes.POINTERDOWN:
+          // Left click -> to draw a sphere
+          if (
+            pointerInfo.event.inputIndex == 2 &&
+            pointerInfo.pickInfo.pickedMesh != null &&
+            (pointerInfo.pickInfo.pickedMesh.id == "Ground" ||
+              pointerInfo.pickInfo.pickedMesh.name == "Ground")
+          ) {
+            coordinates.push(pointerInfo.pickInfo.pickedPoint);
+            const name = "sphere_"+(coordinates.length-1);
+            const sphere = BABYLON.MeshBuilder.CreateSphere(
+              name,
+              { diameter: 0.5, color:BABYLON.Color3.Red()},
+              scene
+            );
+            sphere.position = pointerInfo.pickInfo.pickedPoint;
+            var material = new BABYLON.StandardMaterial("pointMarkerMaterial", scene);
+    material.emissiveColor = new BABYLON.Color3(1, 1, 1); 
+    sphere.material = material;
+          }
+          console.log("POINTER DOWN");
+          break;
+      }
+    }
+  });
+
+  return scene;
+};
 
 var scene = createScene();
+
 engine.runRenderLoop(function () {
-    scene.render();
+  scene.render();
 });
 
 window.addEventListener("resize", function () {
-    engine.resize();
+  engine.resize();
 });
