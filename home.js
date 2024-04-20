3a6d0af6ae9a2ab9bc020a559d3c5fd3d70b500a
Manjari Jain <manjjain@paypal.com>
2024-04-20 11:36:26 +0530

    added draw button

--- home.js
+++ home.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById("groundCanvas"); 
 const engine = new BABYLON.Engine(canvas, true); 
 
+
+var drawMode = false;
+
+const drawButton = document.getElementById("draw");
+drawButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    drawMode = true;
+});
+
+
+
 var createScene = function() {
     var scene = new BABYLON.Scene(engine);
     const camera = new BABYLON.ArcRotateCamera("Camera",  0, Math.PI/4, 30, BABYLON.Vector3(0,0,0), scene);
@@ -9,6 +20,7 @@ var createScene = function() {
     const ground = BABYLON.MeshBuilder.CreateGround("ground", { height:30, width:15 }, scene);
     return scene;
 }
+
 
 var scene = createScene();
 engine.runRenderLoop(function () {
